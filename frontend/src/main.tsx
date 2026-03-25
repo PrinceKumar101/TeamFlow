40ad0aea9dc2e179c7fa5794c3a885091be6e3ed
Prince Kumar <princekumar201926@gmail.com>
2026-03-25 23:19:14 +0530

    commit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,9 +6,9 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 
 createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </Provider>,
+   <Provider store={store}>
+     <QueryClientProvider client={queryClient}>
+       <App />
+     </QueryClientProvider>
+   </Provider>,
 );
